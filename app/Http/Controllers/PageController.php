ae651222729e68da4178589e2a4548652462fe38
msuleman2001 <msuleman2001@gmail.com>
2025-03-20 15:33:07 +0500

    set location number

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -19,6 +19,7 @@ class PageController extends Controller
 
     public function tester(){
         $token = '5a5042a0-2c6e-4b8c-8d8a-0bc33470a9ad';
+        $location_number = "1038525";
         $response = Http::withHeaders([
             'Authorization' => 'Bearer ' . $token, // Include the token in the Authorization header
             'Accept' => 'application/json',       // Optional, ensures the response is in JSON format
